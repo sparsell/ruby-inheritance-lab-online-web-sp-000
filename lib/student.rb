766757b1063fe1f8810936ed4a36370beb595fea
Sonja Parsell <hello@sparsell.com>
2020-05-13 14:35:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+ end
+
+ def learn(knowledge)
+   @knowledge << knowledge
+ end
+
+ def knowledge
+   @knowledge
+ end
 
 end
